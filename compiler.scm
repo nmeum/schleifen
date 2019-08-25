64668e9d4b2234c9673550c200c88571ada758ca
Sören Tempel <soeren+git@soeren-tempel.net>
2019-08-25 20:08:23 +0200

    compiler: Initialize

---
+++ compiler.scm
@@ -0,0 +1,19 @@
+(require-extension comparse)
+(load "parser.scm")
+
+(define (die msg)
+  (let ((port (current-error-port)))
+    (display msg port)
+    (newline port)
+    (exit 1)))
+
+(define (compile-component c)
+  (when c
+    (display (car c))
+    (newline)))
+
+(define (compile-program)
+  (let ((prog (parse parse-commands (current-input-port))))
+    (if prog
+        (for-each compile-component prog)
+        (die "input program is invalid"))))
